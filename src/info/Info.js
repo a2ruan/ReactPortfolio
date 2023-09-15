137067786fd528affbe4debe7b642b233d71d4b0
iqminiclip <30330337+a2ruan@users.noreply.github.com>
2023-09-15 10:50:40 -0400

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -26,7 +26,7 @@ export const info = {
     firstName: "Andy",
     lastName: "Ruan",
     initials: "ar", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Systems Engineer",
+    position: "a Software Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
@@ -41,7 +41,7 @@ export const info = {
         },
         {
             emoji: "💼",
-            text: "Sr. Systems Engineer at AMD"
+            text: "Software Automation Developer at AMD"
         },
         {
             emoji: "📧",
